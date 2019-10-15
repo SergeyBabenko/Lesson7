bf363ff670e8aca7e13959a5a748d80a357b2c79
Sergey Babenko <sergey.babenko.v@outlook.com>
2019-10-15 14:31:47 +0300

    Task 7.4 for Lesson 7. Shows a problem that may occur when using extensions.

--- ru.tinkoff.src/Runner.kt
+++ ru.tinkoff.src/Runner.kt
@@ -2,32 +2,35 @@ enum class Sex {
     MALE, FEMALE, UNKNOWN
 }
 
-class Pet {
+open class Pet {
     var nickname: String = ""
     var age: Int = 0
     var sex: Sex? = Sex.UNKNOWN
     override fun toString(): String = "Pet(nickname=$nickname, age=$age, sex=$sex)"
 }
 
+class Dog : Pet() {
+    override fun toString(): String {
+        return "Dog(nickname=$nickname, age=$age, sex=$sex)"
+    }
+}
+
 fun Pet.isAdult(): Boolean = this.age >= 2
 
-val Pet.respectableNickname: String
-    get() = "Dear ${this.nickname}"
+fun Dog.isAdult(): Boolean = this.age >= 6
 
 fun main() {
-    val dog = Pet().apply {
+    val dog: Pet = Dog().apply {
         this.nickname = "Jessie"
-        this.age = 1
+        this.age = 2
         this.sex = Sex.FEMALE
     }
-    val cat = Pet().apply {
-        this.nickname = "Salem"
-        this.age = 13
+    val niceDog: Dog = Dog().apply {
+        this.nickname = "Rex"
+        this.age = 2
         this.sex = Sex.MALE
     }
 
     println("${dog.nickname} (age: ${dog.age}) is adult? ${dog.isAdult()}")
-    println("${cat.nickname} (age: ${cat.age}) is adult? ${cat.isAdult()}")
-    println(dog.respectableNickname)
-    println(cat.respectableNickname)
+    println("${niceDog.nickname} (age: ${niceDog.age}) is adult? ${niceDog.isAdult()}")
 }
